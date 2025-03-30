1805323c405c5e880e14e372789350a056421ac2
Luiza Mariana <94021382+luizaisok@users.noreply.github.com>
2025-03-30 20:39:18 -0300

    Add files via upload

---
+++ POO/POO4/src/Financas.java
@@ -0,0 +1,17 @@
+/* 
+Imposto a pagar
+
+Um trabalhador autônomo deseja controlar suas finanças, comprou um microcomputador para controlar o rendimento diário de seu trabalho.
+• Toda vez que ele vende um valor maior que o estabelecido pelo regulamento de MEI do estado onde vive (500,00 R$ dia) deve pagar um multa de R$ 0,10 a cada Real excedente.
+• Este trabalhador precisa que você faça um programa em Java que leia o valor de todas as vendas do mês e verifique se há excesso (vendeu mais de 500,00 por dia).
+• Se houver excesso, gravar na variável E (Excesso) e na variável M o valor da multa que o Trabalhador deverá pagar.
+• Caso contrário mostrar tais variáveis com o conteúdo ZERO.
+Desenvolva uma aplicação Java Orientada a Objetos e as classes necessárias para resolver o problema.
+O sistema deve conter um menu com no mínimo as seguintes opções:
+1 - cadastro de vendas
+2 - consulta de imposto a pagar no mês (com base no mês e no ano das vendas).
+0 - Sair. */
+
+public class Financas {
+    
+}
